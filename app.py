698d26bb15f852dd75e4867ad4ae7362422fe1d6
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-02-16 20:30:39 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://stiophan0309:Sleaghach0309@myfirstcluster-s3wmx.mongodb.net/task_manager?retryWrites=true&w=majority')
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT','8080')),
+            debug=True)
