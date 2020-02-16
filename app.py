e2afbcbb714861a443ff3aa1d49dc3885126d3f0
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-02-16 22:53:31 +0000

    Added Add Task page and programming

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://stiophan0309:Sleaghach0309@myfirstcluster-s3wmx.mongodb.net/task_manager?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = 'mongodb+srv://stiophan0309:Sleaghach0309@myfirstcluster-s3wmx.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -14,6 +14,12 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html",
+    categories=mongo.db.categories.find())
 
 
 if __name__ == '__main__':
