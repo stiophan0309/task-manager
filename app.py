fceaae9493c85e7e6e850247adea7083122140c1
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-02-17 21:27:10 +0000

    Added Add/Edit Function

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
@@ -20,6 +20,20 @@ def get_tasks():
 def add_task():
     return render_template("addtask.html",
     categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task=mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories=mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task, categories=all_categories)
+
 
 
 if __name__ == '__main__':
